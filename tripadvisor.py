65212f581313ef6b94dd609d5d63d1e223ac5729
Diego Allen <diegoallen@gmail.com>
2016-06-18 01:10:25 -0400

    Add phone number and locality to restaurants

--- tripadvisor.py
+++ tripadvisor.py
@@ -4,6 +4,15 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def get_details(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return {
+        'locality': soup.find(class_='locality').text[:-2],
+        'phone': getattr(soup.find(class_='phoneNumber'), 'text', '')
+    }
+
+
 def main():
     base_url = 'https://www.tripadvisor.com'
     response = requests.get(base_url + '/Restaurants-g294079-Paraguay.html')
@@ -19,9 +28,10 @@ def main():
             'url': base_url + link['href'],
             'cuisines': [c.text for c in restaurant.select('.cuisine')]
         }
+        current_resto.update(get_details(current_resto['url']))
         results.append(current_resto)
 
-    print json.dumps(results)
+    print json.dumps(results, indent=2)
 
 
 if __name__ == '__main__':
