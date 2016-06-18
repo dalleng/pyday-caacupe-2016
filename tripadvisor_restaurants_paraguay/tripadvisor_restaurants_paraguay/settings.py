106c3dc26970dbe0d02102caebe017c88e7c332f
Diego Allen <diegoallen@gmail.com>
2016-06-18 03:14:16 -0400

    Follow pagination

--- tripadvisor_restaurants_paraguay/tripadvisor_restaurants_paraguay/settings.py
+++ tripadvisor_restaurants_paraguay/tripadvisor_restaurants_paraguay/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'tripadvisor_restaurants_paraguay.spiders'
 #USER_AGENT = 'tripadvisor_restaurants_paraguay (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- tripadvisor_restaurants_paraguay/tripadvisor_restaurants_paraguay/spiders/restaurants.py
+++ tripadvisor_restaurants_paraguay/tripadvisor_restaurants_paraguay/spiders/restaurants.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from urlparse import urljoin
+
 import scrapy
+from w3lib.url import add_or_replace_parameter
+
+from scrapy.utils.response import open_in_browser
 
 
 class RestaurantsSpider(scrapy.Spider):
@@ -11,7 +15,7 @@ class RestaurantsSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        # restaurants = soup.find_all('div', class_='shortSellDetails')
+        # open_in_browser(response)
         for restaurant in response.css('.shortSellDetails'):
             yield {
                 'name': restaurant.css('.property_title::text').extract_first(),
@@ -21,3 +25,13 @@ class RestaurantsSpider(scrapy.Spider):
                 ),
                 'cuisines': restaurant.css('.cuisine::text').extract()
             }
+
+        pagination_url = (
+            'https://www.tripadvisor.com/RestaurantSearch?Action=PAGE'
+            '&geo=294079&ajax=1&sortOrder=popularity&o=a0'
+            '&availSearchEnabled=false'
+        )
+        if not response.css('.nav.next.disabled'):
+            offset = response.meta.get('offset', 0) + 30
+            pagination_url = add_or_replace_parameter(pagination_url, 'o', 'a{}'.format(offset))
+            yield scrapy.Request(pagination_url, meta={'offset': offset})
